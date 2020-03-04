c5e6a8229b3e6119d4d6db5a50a15b314f99bda3
yohan <yohan3287@binus.ac.id>
2020-03-05 00:52:08 +0700

    first commit at 05-03-2020

--- Financial Checkup/ViewController.swift
+++ Financial Checkup/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var activeIncomeTextField: UITextField!
+    @IBOutlet weak var passiveIncomeTextField: UITextField!
+    @IBOutlet weak var debtPaymentTextField: UITextField!
+    @IBOutlet weak var savingTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func clearButtonDidTab(_ sender: Any) {
+        activeIncomeTextField.text = ""
+        passiveIncomeTextField.text = ""
+        debtPaymentTextField.text = ""
+        savingTextField.text = ""
+    }
+    
+    @IBAction func calculateButtonDidTab(_ sender: UIButton) {
+        guard let activeIncomeTmp = activeIncomeTextField.text else{
+            return
+        }
+        let activeIncome = Int(activeIncomeTmp)
+        
+        guard let passiveIncomeTmp = passiveIncomeTextField.text else{
+            return
+        }
+        let passiveIncome = Int(passiveIncomeTmp)
+        
+        guard let debtPaymentTmp = debtPaymentTextField.text else{
+            return
+        }
+        let debtPayment = Int(debtPaymentTmp)
+        
+        guard let savingTmp = savingTextField.text else{
+            return
+        }
+        let saving = Int(savingTmp)
+        
+        print(activeIncome)
+        print(passiveIncome)
+        print(debtPayment)
+        print(saving)
+        
+        let a = Int(unwrap(activeIncomeTextField.text))
+        let b = Int(unwrap(passiveIncomeTextField.text))
+        let no = a + b
+        print(no)
+    }
+    
+    func unwrap(_ arg : String?) -> String{
+        guard let temp = arg else {
+            return ""
+        }
+        return temp
+    }
+    
 }
 
