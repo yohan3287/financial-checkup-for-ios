ebd842e74b327a7300f472c25147c9000c998526
yohan <yohan3287@binus.ac.id>
2020-03-05 18:18:46 +0700

    segmented control berhasil

--- Financial Checkup/ViewController.swift
+++ Financial Checkup/ViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var calculatorView: UIView!
     
     @IBOutlet weak var activeIncomeTextField: UITextField!
     @IBOutlet weak var passiveIncomeTextField: UITextField!
     @IBOutlet weak var debtPaymentTextField: UITextField!
     @IBOutlet weak var savingTextField: UITextField!
     
+    @IBOutlet weak var spendingResult: UILabel!
+    @IBOutlet weak var debtPaymentResult: UILabel!
+    @IBOutlet weak var savingResult: UILabel!
+    @IBOutlet weak var financialFreedomResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func changeSegmentedControlDidTab(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+//            print("Calculator")
+            calculatorView.isHidden = false
+            break
+        case 1:
+//            print("Result")
+            calculatorView.isHidden = true
+            break
+        default:
+            break
+        }
+    }
+    
     @IBAction func clearButtonDidTab(_ sender: Any) {
         activeIncomeTextField.text = ""
         passiveIncomeTextField.text = ""
@@ -27,42 +50,69 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateButtonDidTab(_ sender: UIButton) {
-        guard let activeIncomeTmp = activeIncomeTextField.text else{
-            return
-        }
-        let activeIncome = Int(activeIncomeTmp)
+//        guard let activeIncomeTmp = activeIncomeTextField.text else{
+//            return
+//        }
+//        guard let activeIncome = Int(activeIncomeTmp) else{
+//            return
+//        }
         
-        guard let passiveIncomeTmp = passiveIncomeTextField.text else{
-            return
-        }
-        let passiveIncome = Int(passiveIncomeTmp)
+//        guard let passiveIncomeTmp = passiveIncomeTextField.text else{
+//            return
+//        }
+//        guard let passiveIncome = Int(passiveIncomeTmp) else{
+//            return
+//        }
         
-        guard let debtPaymentTmp = debtPaymentTextField.text else{
-            return
-        }
-        let debtPayment = Int(debtPaymentTmp)
+//        guard let debtPaymentTmp = debtPaymentTextField.text else{
+//            return
+//        }
+//        guard let debtPayment = Int(debtPaymentTmp) else{
+//            return
+//        }
         
-        guard let savingTmp = savingTextField.text else{
-            return
-        }
-        let saving = Int(savingTmp)
+//        guard let savingTmp = savingTextField.text else{
+//            return
+//        }
+//        guard let saving = Int(savingTmp) else{
+//            return
+//        }
+                
+        let activeIncome = unwrapStrToInt(activeIncomeTextField.text)
+        let passiveIncome = unwrapStrToInt(passiveIncomeTextField.text)
+        let debtPayment = unwrapStrToInt(debtPaymentTextField.text)
+        let saving = unwrapStrToInt(savingTextField.text)
+        let spending = (activeIncome + passiveIncome) - (debtPayment + saving)
         
-        print(activeIncome)
-        print(passiveIncome)
-        print(debtPayment)
-        print(saving)
+//        print(spending)
+//        print(activeIncome)
+//        print(passiveIncome)
+//        print(debtPayment)
+//        print(saving)
+//        print("\n")
         
-        let a = Int(unwrap(activeIncomeTextField.text))
-        let b = Int(unwrap(passiveIncomeTextField.text))
-        let no = a + b
-        print(no)
+        let spendingPercentage = 100 * spending / (activeIncome + passiveIncome)
+        let debtPaymentPercentage = 100 * debtPayment / (activeIncome + passiveIncome)
+        let savingPercentage = 100 * saving / (activeIncome + passiveIncome)
+        let passiveIncomePercentage = 100 * passiveIncome / (spending + debtPayment)
+        
+//        print(spendingPercentage)
+//        print(debtPaymentPercentage)
+//        print(savingPercentage)
+//        print(passiveIncomePercentage)
+//        print(spendingPercentage + debtPaymentPercentage + savingPercentage)
+        
+        
     }
     
-    func unwrap(_ arg : String?) -> String{
-        guard let temp = arg else {
-            return ""
+    func unwrapStrToInt(_ arg : String?) -> Int{
+        guard let temp1 = arg else {
+            return 0
         }
-        return temp
+        guard let temp2 = Int(temp1) else{
+            return 0
+        }
+        return temp2
     }
     
 }
